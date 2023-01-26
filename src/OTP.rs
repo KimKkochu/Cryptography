da35ef0abf28ebf56b1ae58ba3f602f5be67eb47
KimKkochu <kimkkochu69@gmail.com>
2023-01-26 22:06:51 +0900

    add decryptrion

--- src/OTP.rs
+++ src/OTP.rs
@@ -1,3 +1,7 @@
-pub fn encryption(plaintext: i32, key: i32) -> i32 {
+pub fn encryption(plaintext: &i32, key: &i32) -> i32 {
 	plaintext ^ key
+}
+
+pub fn decryptrion(ciphertext: &i32, key: &i32) -> i32 {
+	ciphertext ^ key
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 #![allow(non_snake_case)]
 
-use Cryptography::OTP::encryption;
+use Cryptography::OTP;
 
 fn main() {
-    let i = encryption(0b01101101, 0b10110100);
-    println!("{:b}",i);
+    let key: i32 = 0b10110100;
+    let plaintext: i32 = 0b01101101;
+    let ciphertext = OTP::encryption(&plaintext, &key);
+    let decodetext = OTP::decryptrion(&ciphertext, &key);
+    
+    println!("Ciphertext: {:b}, decodetext: {:b}", ciphertext, decodetext);
 }
